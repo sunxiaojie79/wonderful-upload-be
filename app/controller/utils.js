2035b8d0bb79c571a20647cb62451182164271b9
sunxiaojie79 <297280911@qq.com>
2020-08-19 00:32:47 +0800

    feat:图片验证码实现

--- app/controller/utils.js
+++ app/controller/utils.js
@@ -4,10 +4,18 @@ const Controller = require('egg').Controller;
 const svgCaptcha = require('svg-captcha');
 class UtilsController extends Controller {
   async captcha() {
-    const captcha = svgCaptcha.create();
-    console.log(captcha)
+    const captcha = svgCaptcha.create({
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 40,
+      noise:3,
+    });
+    console.log('captcha', captcha.text);
     const { ctx } = this;
-    ctx.body = captcha;
+    ctx.response.type = 'image/svg+xml';
+    ctx.session.captcha = captcha.text;
+    ctx.body = captcha.data;
   }
 }
 
