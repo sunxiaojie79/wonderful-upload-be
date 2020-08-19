3665a542c160624c307fc0b9c16dd1e78d232f68
sunxiaojie79 <297280911@qq.com>
2020-08-20 02:00:47 +0800

    feat:egg规范制定

--- app/controller/user.js
+++ app/controller/user.js
@@ -6,9 +6,9 @@ class UserController extends BaseController {
   async register() {
     return this.message('注册成功');
   }
-  async login () { return this.message('注册成功');}
+  async login() { return this.message('注册成功'); }
   async info() { return this.message('注册成功'); }
-  async verify () { return this.message('注册成功'); }
+  async verify() { return this.message('注册成功'); }
 }
 
 module.exports = UserController;
