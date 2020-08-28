1a7100a94de07cd82162f5b69dc4624d276d9489
sunxiaojie79 <297280911@qq.com>
2020-08-29 00:35:30 +0800

    feat:登录认证

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,6 +1,7 @@
 'use strict';
 const md5 = require('md5');
 const BaseController = require('./base.js');
+const jwt = require('jsonwebtoken');
 
 const HashSalt = 'jiadianyanba';
 const createRule = {
@@ -18,28 +19,51 @@ class UserController extends BaseController {
       return this.error('参数校验失败', -1, e.errors);
     }
     const { email, captcha, nickname, passwd } = ctx.request.body;
-    if (captcha.toUpperCase() === ctx.session.captcha.toUpperCase()) {
-      if (await this.checkEmail(email)) {
-        this.error('邮箱重复');
-      } else {
-        const res = await ctx.model.User.create({
-          email,
-          nickname,
-          passwd: md5(passwd + HashSalt),
-        });
-        if (res._id) {
-          return this.message('注册成功');
-        }
-      }
+    if (captcha.toUpperCase() !== ctx.session.captcha.toUpperCase()) {
+      return this.error('验证码错误');
     }
-    return this.error('验证码错误');
-
+    if (await this.checkEmail(email)) {
+      return this.error('邮箱重复');
+    }
+    const res = await ctx.model.User.create({
+      email,
+      nickname,
+      passwd: md5(passwd + HashSalt),
+    });
+    if (res._id) {
+      return this.message('注册成功');
+    }
   }
   async checkEmail(email) {
     const user = await this.ctx.model.User.findOne({ email });
     return user;
   }
-  async login() { return this.message('注册成功'); }
+  async login () {
+    const { ctx, app } = this;
+    const { email, captcha, passwd } = ctx.request.body;
+    console.log('email', email)
+    if (captcha.toUpperCase() !== ctx.session.captcha.toUpperCase()) {
+      return this.error('验证码错误');
+    }
+    const user = await this.ctx.model.User.findOne({
+      email,
+      passwd: md5(passwd + HashSalt)
+    });
+    if (!user) { 
+      return this.error('账号密码错误');
+    }
+    const token = jwt.sign({
+      _id: user._id,
+      email,
+    }, app.config.jwt.secret, {
+      expiresIn: '1h',
+    })
+    this.success({
+      email,
+      nickname: user.nickname,
+      token,
+    })
+   }
   async info() { return this.message('注册成功'); }
   async verify() { return this.message('注册成功'); }
 }
